75d40b733b311ff4b9f95794630723ad6b1d9202
yuxin <yuxinchai3111@gmail.com>
2025-07-22 02:16:28 +0000

    FINISHED

--- Part2/ex17.py
+++ Part2/ex17.py
@@ -3,4 +3,4 @@ import random
 adjectives = ["Sneaky", "Caring", "Sly", "Fearless", "Rich", "Hungry"]
 animals = ["Fish", "Axolotl", "Dragon", "Rabbit", "Bird", "Ladybug"]
 print(name + ", your codename is: " + random.choice(adjectives) + " " + random.choice(animals))
-print("Your lucky number is: " + random.randint(0,100))
+print("Your lucky number is: " + str(random.randint(0,100)))
